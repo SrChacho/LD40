7fd9a76d357edb1393a7e90a4a8ab9fd30acd4d4
JuanLagu <juan_lagu_@hotmail.com>
2017-12-02 15:16:12 +0000

    Road movement and UI

---
+++ Assets/Scripts/Scrolling.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour {
+
+    public float speed = 1;
+    public float limit;
+    public float newX;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+
+        transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+
+        if(transform.position.x < limit)
+        {
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+
+    }
+}
