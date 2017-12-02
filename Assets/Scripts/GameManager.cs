e2040ab136d8fcfe7a94dda74e36892b493c8017
Fabian <fabiancm1995@gmail.com>
2017-12-02 21:45:40 +0000

    death animation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour {
     public float score = 0;
     public float gameSpeed = 1;
     public Text scoreText;
+    public GameObject cup;
     
     void Awake()
     {
@@ -35,6 +36,16 @@ public class GameManager : MonoBehaviour {
     public void GameOver()
     {
         print("Game Over");
+        GameObject player = GameObject.Find("Player");
+        Vector2 pos = player.transform.position;
+        Destroy(player.gameObject);
+        GameObject cupInstance = Instantiate(cup, pos, new Quaternion(0,0,0,0));
+        cupInstance.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 20, ForceMode2D.Impulse);
+        Invoke("StopGame", 1.12f);
+    }
+
+    void StopGame(){
+        gameSpeed = 0;
     }
 
 }
